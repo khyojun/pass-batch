ee1295bd5d8bff4024f33a8563823f01ea52479b
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-04-30 18:21:49 +0900

    first commit

---
+++ src/main/java/com/example/fastcampuspt/FastcampusPtApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastcampuspt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusPtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusPtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastcampuspt/FastcampusPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastcampuspt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusPtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
